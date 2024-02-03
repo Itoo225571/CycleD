827884d8e54399153e59ac7ee6cb30f8d704dc9f
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-03 21:45:01 +0900

    path関数の説明

--- MyProject0/MyApp0/urls.py
+++ MyProject0/MyApp0/urls.py
@@ -6,6 +6,9 @@ from MyApp0 import views
 app_name="MyApp0"
 book="book/"
 urlpatterns=[
+    #path(指定URL,送信するデータ,URL名)
+    #<型名(int,str等):引数名>
+    #URL名を付けることで、URLを変更しても使える
     path("book/",views.book_list,name="book_list"),
     path("book/add/",views.book_edit,name="book_add"),
     path("book/mod/<int:book_id>/",views.book_edit,name="book_mod"),
