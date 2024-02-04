dd9492d88766285de85f71c7484a12717fbc7612
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-04 13:08:55 +0900

    Fromについての解説
    参考
    https://daeudaeu.com/django-form/

--- MyProject0/MyApp0/forms.py
+++ MyProject0/MyApp0/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
 from MyApp0.models import Book
 
+# Form...データ送信のためのユーザーインターフェイス,フィールド(入力欄)＋ボタンから成る
+
 class BookForm(ModelForm):
+    #Metaを編集することで、Formに追加情報(fields等)を渡す
     class Meta:
         model=Book
+        #本来は、name=models.CharField()みたく登録するが、modelsは既にmodels.pyで定義されてる
         fields=("name","publish","page")
