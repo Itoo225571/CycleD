fbc0f2d145c8ca4fd5ce623bad8df9d39aaa410a
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-05 12:01:39 +0000

    メモ追加

--- MyProject0/MyApp0/forms.py
+++ MyProject0/MyApp0/forms.py
@@ -4,13 +4,13 @@ from MyApp0.models import Book,Evaluation
 # Form...データ送信のためのユーザーインターフェイス,フィールド(入力欄)＋ボタンから成る
 
 class BookForm(ModelForm):
-    #Metaを編集することで、Formに追加情報(fields等)を渡す
-    class Meta:
-        model=Book
-        #本来は、name=models.CharField()みたく登録するが、modelsは既にmodels.pyで定義されてる
-        fields=("name","publish","page")
+        #Metaを編集することで、Formに追加情報(fields等)を渡す
+        class Meta:
+                model=Book
+                #本来は、name=models.CharField()みたく登録するが、modelsは既にmodels.pyで定義されてる
+                fields=("name","publish","page")
         
 class EvaluationForm(ModelForm):
-    class Meta:
-        model=Evaluation
-        fields=("star","comment",)
+        class Meta:
+                model=Evaluation
+                fields=("star","comment",)
