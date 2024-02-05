ec4d8f3c391110f7454a0264bc681cd6b55e0c08
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-05 12:02:03 +0000

    メモ追加2

--- customadmin/AppManagement/models.py
+++ customadmin/AppManagement/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name=models.CharField(max_length=256)
+    publish=models.CharField(max_length=64)
+    author=
+    
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+    name=models.CharField(max_length=256)
+    
