cd79a0762260ac6231b856057962aedb81df03ec
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-07 05:20:28 +0000

    adminへモデルクラスを追加

--- customadmin/AppManagement/admin.py
+++ customadmin/AppManagement/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# from .models import モデルクラス
+# admin.site.register(モデルクラス,ModelAdminのサブクラス)で管理(admin)画面で管理するモデルクラスを登録
+
+#ここでサブクラスの設定を行うことで、表示するものを決める
+class SubModelAdmin(admin.ModelAdmin):
+    list_display=("__str__","author")
+
+from .models import Book,Author,Score
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Score)
