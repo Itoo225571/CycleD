1bf359d6976c945fe040eff098f484da038dfed5
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-07 11:31:24 +0000

    score追加

--- customadmin/AppManagement/admin.py
+++ customadmin/AppManagement/admin.py
@@ -3,11 +3,22 @@ from django.contrib import admin
 # from .models import モデルクラス
 # admin.site.register(モデルクラス,ModelAdminのサブクラス)で管理(admin)画面で管理するモデルクラスを登録
 
-#ここでサブクラスの設定を行うことで、表示するものを決める
-class SubModelAdmin(admin.ModelAdmin):
-    list_display=("__str__","author")
-
 from .models import Book,Author,Score
-admin.site.register(Book)
+
+#ここでサブクラスの設定を行うことで、表示するものを決める
+class BookAdmin(admin.ModelAdmin):
+    list_display=("__str__","author","publish","is_favorite")    
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display=("book","score")
+    exclude=("score",)
+    
+    #saveする際の設定
+    def save_model(self,request,obj,form,change):
+        obj.score=(obj.art+obj.story)/2
+        super().save_model(request,obj,form,change)
+
+
+admin.site.register(Book,BookAdmin)
 admin.site.register(Author)
-admin.site.register(Score)
+admin.site.register(Score,ScoreAdmin)
--- customadmin/AppManagement/models.py
+++ customadmin/AppManagement/models.py
@@ -23,6 +23,7 @@ class Score(models.Model):
     book=models.OneToOneField(Book,on_delete=models.CASCADE)#どのBookか
     art=models.IntegerField()#絵に対する評価/100
     story=models.IntegerField()#お話に対する評価/100
+    score=models.IntegerField()#総評
     comment=models.CharField(max_length=2048)#コメント
     
     def __str__(self):
