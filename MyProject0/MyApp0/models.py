b02ceaec6637586bf39c975bde9876ebf7cdb4c2
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-07 11:31:02 +0000

    Score追加

--- MyProject0/MyApp0/models.py
+++ MyProject0/MyApp0/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     name=models.CharField("書籍名",max_length=100)
     publish=models.CharField("出版社",max_length=100,blank=True)
-    page=models.IntegerField("ページ数",blank=True,default=0)
+    #page=models.IntegerField("ページ数",blank=True,default=0)
     
     def __str__(self):
         return self.name
@@ -12,7 +12,8 @@ class Book(models.Model):
 #評価    
 class Evaluation(models.Model):
     book=models.ForeignKey(Book,verbose_name="書籍",related_name="evaluation",on_delete=models.CASCADE)
-    star=models.IntegerField("星",default=0)
+    score=models.IntegerField("評価")
+    star=models.BooleanField(verbose_name="お気に入り",default=False)
     comment=models.TextField("コメント",blank=True)
     
     def __str__(self):
