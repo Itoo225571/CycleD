ef80450f140b2dd3d0f72dc2654314e247d7580b
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-09 08:20:12 +0000

    Models:model追加＋adminで管理可能にする

--- cycled_project/diary/models.py
+++ cycled_project/diary/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username=models.CharField(max_length=128)
+    email_address=models.EmailField()
+    # password=models.CharField(max_length=128)
+    # creation_date=models.DateField()
+    # last_login_date=models.DateField()
+    
+    def __str__(self):
+        return self.username
+    
+class Diary(models.Model):
+    content=models.TextField()
+    # place=models.CharField()
+    creation_date=models.DateField()
+    # last_update_date=models.DateField()
+    # publish_flag=models.BooleanField(default=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    # tags=models.CharField(max_length=64)
+    # image=models.ImageField(upload_tp="images/")
+    
+    def __str__(self):
+        return self.creation_date
