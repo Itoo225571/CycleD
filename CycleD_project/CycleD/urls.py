76707990156d16178b707132e11311e43a509df8
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-09 15:50:19 +0900

    Update urls.py

--- CycleD_project/CycleD/urls.py
+++ CycleD_project/CycleD/urls.py
@@ -1,18 +1,16 @@
 """
-URL configuration for CycleD project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
+    CycleDプロジェクト内のURL構成設定ファイル
+    urlpatternsリストは、URLをビューにルーティングします。詳細については、以下を参照してください：
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    例：関数ベースのビュー
+        1. インポートを追加します： from my_app import views
+        2. urlpatternsにURLを追加します： path('', views.home, name='home')
+    クラスベースのビュー
+        1. インポートを追加します： from other_app.views import Home
+        2. urlpatternsにURLを追加します： path('', Home.as_view(), name='home')
+    別のURLconfを含める
+        1. include()関数をインポートします： from django.urls import include, path
+        2. urlpatternsにURLを追加します： path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
