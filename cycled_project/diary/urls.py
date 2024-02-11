f42cf1f90a00eeb00a49d461c539cef9e755c274
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-11 14:41:20 +0000

    Views：diary新規作成機能追加

--- cycled_project/diary/urls.py
+++ cycled_project/diary/urls.py
@@ -6,6 +6,7 @@ app_name="diary"
 urlpatterns = [
     path("",views.UserIndexView.as_view(),name="user_index"),
     path("<int:pk>/",views.UserDetailView.as_view(),name="user_detail"),
-    path("<int:user_id>/<int:diary_id>/write",views.diary_write,name="diary_write"),
-    
+    path("<int:user_id>/<int:pk>/update",views.DiaryUpdateView.as_view(),name="diary_update"),
+    path("<int:user_id>/create",views.DiaryCreateView.as_view(),name="diary_create"),
+    path("<int:user_id>/write",views.diary_write,name="diary_write"),
 ]
