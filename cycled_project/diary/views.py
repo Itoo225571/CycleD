f1d38182f6c1cb63513926b461b20d1b1b7335d2
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-11 08:34:27 +0000

    View：classへ変更+write追加 HTML：detailからwriteへ

--- cycled_project/diary/views.py
+++ cycled_project/diary/views.py
@@ -1,25 +1,49 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
 
 from .models import User,Diary
 
 from datetime import datetime
 
-def user_index(request):
-    user_list=User.objects.order_by("id")
-    # output=",".join([user.name for user in user_list])
-    
-    # template=loader.get_template("diary/user_index.html")
-    template="diary/user_index.html"
-    context={
-            "user_list":user_list,
-            }
-    return render(context=context,
-                  request=request,
-                  template_name=template)
+class UserIndexView(generic.ListView):
+        template_name="diary/user_index.html"
+        context_object_name="user_list"
+        # def get_queryset(self):
+        #         return User.objects.order_by("id")
+        model=User
 
-    
-def user_detail(request,user_id):
+        
+class UserDetailView(generic.DetailView):
+        model=User
+        template_name="diary/user_detail.html"
+        
+class DiaryWriteView(generic.UpdateView):
+        pass
+
+def diary_write(request,user_id):
         user=get_object_or_404(User,pk=user_id)
-        return render(request=request,
-                      template_name="diary/user_detail.html",
-                      context={"user":user})
+        try:
+                content=user.diary_set.get(pk=request.POST["diary_write"])
+        except(KeyError,Diary.DoesNotExist):
+                error_message="なんにもかいてないよぅ"
+                return render(request,
+                              "diary/user_detail.html",
+                              {"user":user,"error_message":error_message},
+                              )
+        else:
+                content.save()
+        
+        return HttpResponseRedirect(reverse("diary:diary_check"),
+                                    args=(user.id)
+                                    )
+        
+def diary_check(request,user_id):
+        user=get_object_or_404(User,pk=user_id)
+        return render(request,
+                      "diary/diary_check.html",
+                      {"user":user}
+                      )
