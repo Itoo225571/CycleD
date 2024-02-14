7d58c165899c1e988c0030ef271b729ffe5ed776
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-14 10:15:50 +0000

    Admin：useradmin追加 Model：userに色々追加

--- cycled_project/diary/admin.py
+++ cycled_project/diary/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import User,Diary
 
+class UserAdmin(admin.ModelAdmin):
+    fields=["name","email_address",]
+
 admin.site.register(User)
 admin.site.register(Diary)
