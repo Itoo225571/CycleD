cf30546bde93ac98353d2cf9ef1fc7eb7a616bc8
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-15 13:34:39 +0000

    mapのテスト

---
+++ Tests/test.py
@@ -0,0 +1,12 @@
+import folium
+from geopy.geocoders import Nominatim
+
+geolocator=Nominatim(user_agent="geoapiExcercises")
+geolocation=geolocator.geocode("幕張駅")
+location=[geolocation.latitude,geolocation.longitude]
+
+m=folium.Map(location=location,zoom_start=16)
+folium.Marker(location).add_to(m)
+m.save("map.html")
+
+print(location)
