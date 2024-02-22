90b162a2320a095b94487782557fd6c953cf7559
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-02-22 10:26:20 +0000

    ブランチ実験

--- cycled_project/diary/urls.py
+++ cycled_project/diary/urls.py
@@ -3,7 +3,18 @@ from django.urls import path
 from . import views
 
 app_name="diary"
+# urlpatterns = [
+#     path("",views.UserIndexView.as_view(),name="user_index"),
+#     path("<int:pk>/",views.UserDetailView.as_view(),name="user_detail"),
+#     path("<int:pk>/<int:diary_id>/update",views.DiaryUpdateView.as_view(),name="diary_update"),
+#     path("<int:user_id>/<int:diary_id>/write",views.diary_write,name="diary_write"),
+#     path("<int:user_id>/create",views.DiaryCreateView.as_view(),name="diary_create"),
+#     path("<int:user_id>/write",views.diary_write,name="diary_write"),
+    
+# ]
+
 urlpatterns = [
+<<<<<<< HEAD
     path("",views.UserIndexView.as_view(),name="user_index"),
     path("<int:pk>/",views.UserDetailView.as_view(),name="user_detail"),
     path("<int:pk>/<int:diary_id>/update",views.DiaryUpdateView.as_view(),name="diary_update"),
@@ -13,3 +24,7 @@ urlpatterns = [
     
 ]
 
+=======
+    path("/",views)
+]
+>>>>>>> 82b088b (ブランチ実験)
