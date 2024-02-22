d68d88dba6b8e321164890b78fa202dd61fabaa3
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-22 14:34:44 +0000

    model,urlを設計時のものへ変更

--- cycled_project/diary/forms.py
+++ cycled_project/diary/forms.py
@@ -4,10 +4,12 @@ from django.forms import ModelForm
 from diary.models import User,Diary
 
 class UserForm(ModelForm):
-    pass
+    class Meta:
+        model=User
+        fields=["name","email","password","address",]
 
 class DiaryForm(ModelForm):
     class Meta:
         model=Diary
-        fields=("place",)
+        fields=["date","name_place","image","is_publish","comment",]
         
