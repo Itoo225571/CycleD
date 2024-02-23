49766eb1adda34095b5de87369ca6f702d3476b8
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-02-23 07:09:52 +0000

    Views：views変更

--- cycled_project/diary/views.py
+++ cycled_project/diary/views.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render,get_object_or_404,redirect
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse,HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse,reverse_lazy
 
@@ -10,3 +10,45 @@ from .forms import UserForm,DiaryForm
 
 # from datetime import datetime
 
+class ToppageView(generic.TemplateView):
+    template_name="dairy/toppage.html"
+    
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        return super().get(request, *args, **kwargs)
+toppage=ToppageView.as_view()
+
+class SigninView(generic.FormView):
+    pass
+signin=SigninView.as_view()
+
+class SignupView(generic.CreateView):
+    pass
+signup=SignupView.as_view()
+
+class UserProfileView(generic.TemplateView):
+    pass
+user_profile=UserProfileView.as_view()
+
+class UserEditView(generic.UpdateView):
+    pass
+user_edit=UserEditView.as_view()
+
+class DiaryView(generic.TemplateView):
+    pass
+diary=DiaryView.as_view()
+
+class DiaryNew(generic.CreateView):
+    pass
+diary_new=DiaryNew.as_view()
+
+class DiaryEdit(generic.UpdateView):
+    pass
+diary_edit=DiaryEdit.as_view()
+
+class DiaryDelete(generic.DeleteView):
+    pass
+diary_delete=DiaryDelete.as_view()
+
+class CalendarView(generic.TemplateView):
+    pass
+calendar=CalendarView.as_view()
