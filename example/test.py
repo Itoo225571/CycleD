686bf2de8e2f711bbfa4ad186d4bec59d7a99ef1
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-04-13 14:57:22 +0000

    TOPとHOME分解

--- example/test.py
+++ example/test.py
@@ -1,13 +1,7 @@
-import folium
-from geopy.geocoders import Nominatim
-
-geolocator=Nominatim(user_agent="geoapiExcercises")
-geolocation=geolocator.geocode("幕張駅")
-
-location=[geolocation.latitude,geolocation.longitude]
-
-m=folium.Map(location=location,zoom_start=16)
-folium.Marker(location).add_to(m)
-m.save("map.html")
-
-print(location)
+import requests
+geo_request_url = 'https://get.geojs.io/v1/ip/geo.json'
+data = requests.get(geo_request_url).json()
+print(data['latitude'])
+print(data['longitude'])
+print(data['country'])
+print(data['city'])
