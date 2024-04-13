cc5c8212e769987fbbfca40a0efda2c7a4a631f6
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-04-13 12:51:58 +0000

    weather_repot完成

--- cycled_project/diary/subs/weather_report.py
+++ cycled_project/diary/subs/weather_report.py
@@ -3,10 +3,18 @@ import geocoder.osm
 
 import urllib.parse
 import pandas as pd
+import json
+from os import getcwd
 from decimal import Decimal, ROUND_HALF_UP
 from datetime import datetime
 
 class WeatherReport:
+
+	with open("subs/weather_category.json",mode="rt") as f:
+			weather_categories = json.load(f)
+	# 文字列から整数にキーを変換する
+	weather_categories = {int(key): value for key, value in weather_categories.items()}
+	
 	def __init__(self,*args):
 		if len(args)==2 and isinstance(args[0],(float,int)) and isinstance(args[1],(float,int)):
 			latitude,longtitude=args
@@ -37,92 +45,24 @@ class WeatherReport:
 		self.data["daily"]["time"]=self.decode_time(self.data["daily"]["time"])
 		self.data["current"]["time"]=self.decode_time(self.data["current"]["time"])
 
-		self.data["hourly"]["weather"]=self.decode_weather_category(self.data["hourly"]["weather_code"])
-		self.data["daily"]["weather"]=self.decode_weather_category(self.data["daily"]["weather_code"])
-		self.data["current"]["weather"]=self.decode_weather_category(self.data["current"]["weather_code"])
+		self.data["hourly"]["weather"],self.data["hourly"]["weather_img"]=self.decode_weather_category(self.data["hourly"]["weather_code"])
+		self.data["daily"]["weather"],self.data["daily"]["weather_img"]=self.decode_weather_category(self.data["daily"]["weather_code"])
+		self.data["current"]["weather"],self.data["current"]["weather_img"]=self.decode_weather_category(self.data["current"]["weather_code"])
 
 		self.data["hourly"]["wind_direction_10m_ja"]=self.en_to_ja_direction(self.data["hourly"]["wind_direction_10m"])
 		self.data["current"]["wind_direction_10m_ja"]=self.en_to_ja_direction(self.data["current"]["wind_direction_10m"])
 
-		self.df_hourly = pd.DataFrame(index=self.data["hourly"]["time"],data=self.data["hourly"])
-		self.df_daily = pd.DataFrame(index=self.data["daily"]["time"],data=self.data["daily"])
-		self.df_current=pd.DataFrame(index=[self.data["current"]["time"]],data=self.data["current"])
-
 	"""__コード解読用メソッド__"""
 	def decode_weather_category(self,codes):
-		weather_categories = {
-        0: "Clear",
-        1: "Cloudy",
-        2: "Cloudy",
-        3: "Cloudy",
-        40: "Mist",
-        41: "Mist",
-        42: "Mist",
-        43: "Mist",
-        44: "Mist",
-        45: "Mist",
-        46: "Mist",
-        47: "Mist",
-        48: "Mist",
-        49: "Mist",
-        50: "Light Rain",
-        51: "Light Rain",
-        52: "Light Rain",
-        53: "Light Rain",
-        54: "Light Rain",
-        55: "Light Rain",
-        56: "Light Rain",
-        57: "Light Rain",
-        58: "Light Rain",
-        59: "Light Rain",
-        60: "Rain",
-        61: "Rain",
-        62: "Rain",
-        63: "Rain",
-        64: "Rain",
-        65: "Rain",
-        66: "Rain",
-        67: "Rain",
-        68: "Rain",
-        69: "Rain",
-        70: "Snow",
-        71: "Snow",
-        72: "Snow",
-        73: "Snow",
-        74: "Snow",
-        75: "Snow",
-        76: "Snow",
-        77: "Snow",
-        78: "Snow",
-        79: "Snow",
-        80: "Showers",
-        81: "Showers",
-        82: "Showers",
-        83: "Showers",
-        84: "Showers",
-        85: "Snow Showers",
-        86: "Snow Showers",
-        87: "Snow Showers",
-        88: "Snow Showers",
-        89: "Snow Showers",
-        90: "Thunderstorm",
-        91: "Thunderstorm",
-        92: "Thunderstorm",
-        93: "Thunderstorm",
-        94: "Thunderstorm",
-        95: "Thunderstorm",
-        96: "Thunderstorm",
-        97: "Thunderstorm",
-        98: "Thunderstorm",
-        99: "Thunderstorm"
-    }
-		new_codes=[]
-		if isinstance(codes, list):
-			for code in codes:
-				new_codes.append(weather_categories.get(code, None))
-		elif isinstance(codes, int):
-			return weather_categories.get(codes, None)
-		return new_codes
+		descriptions=[]
+		imgs=[]
+		if isinstance(codes,int):
+			codes=[codes]
+		for code in codes:
+			weather=self.weather_categories.get(code, None)
+			descriptions.append(weather["description"])
+			imgs.append(weather["img"])
+		return descriptions,imgs
 	
 	def decode_time(self,times):
 		new_times=[]
@@ -142,14 +82,16 @@ class WeatherReport:
 	"""__翻訳用__"""
 	def en_to_ja_weather(self,weather):
 		weather_dict = {
-		"Clear": "晴れ",
+		"Clear": "快晴",
+		"Mainly_Sunny":"晴れ",
+		"Partly_Cloudy":"一部曇り",
 		"Cloudy": "曇り",
 		"Mist": "霧",
-		"Light Rain": "小雨",
+		"Light_Rain": "小雨",
 		"Rain": "雨",
 		"Snow": "雪",
 		"Showers": "時々雨",
-		"Snow Showers": "時々雪",
+		"Snow_Showers": "時々雪",
 		"Thunderstorm": "雷"
 		}
 		new_weathers=[]
@@ -180,12 +122,14 @@ class WeatherReport:
 
 	"""__デバッグ用__"""
 	def to_csv(self):
+		self.df_hourly = pd.DataFrame(index=self.data["hourly"]["time"],data=self.data["hourly"])
+		self.df_daily = pd.DataFrame(index=self.data["daily"]["time"],data=self.data["daily"])
+		self.df_current=pd.DataFrame(index=[self.data["current"]["time"]],data=self.data["current"])
+	
 		self.df_hourly.to_csv(str(self.location_name)+"_hourly.csv")
 		self.df_daily.to_csv(str(self.location_name)+"_daily.csv")
 		self.df_current.to_csv(str(self.location_name)+"_current.csv")
-		
-	
-if __name__=="__main__":
-	# w=WeatherReport(35.72475432967049,139.58134714317478)
-	w=WeatherReport("関町東")
+
+if __name__=="__main__":    	
+	w=WeatherReport("関町")
 	w.to_csv()
