ea038f871d14179341110af0c9fdc2c54fb5bd3b
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-04-13 12:51:58 +0000

    weather_repot完成

---
+++ cycled_project/diary/diary_weather_report.py
@@ -0,0 +1,12 @@
+from subs.weather_report import WeatherReport
+
+class DiaryWeatherReport(WeatherReport):
+	def __init__(self, *args):
+		super().__init__(*args)
+		self.data["hourly"]["weather_img"]=["diary:"+name for name in self.data["hourly"]["weather_img"]]
+		self.data["daily"]["weather_img"]=["diary:"+name for name in self.data["daily"]["weather_img"]]
+		self.data["current"]["weather_img"]=["diary:"+name for name in self.data["current"]["weather_img"]]
+		
+if __name__=="__main__":    	
+	w=DiaryWeatherReport("関町")
+	# w.to_csv()
--- subs/weather_by_location.py
+++
@@ -1,7 +0,0 @@
-import geocoder
-
-if __name__=="__main__":
-    place_names=["練馬","東京タワー","荒川","明治大学生田",]
-    for place in place_names:
-        ret=geocoder.osm(place,timeout=5.0)
-        print(ret.address)
