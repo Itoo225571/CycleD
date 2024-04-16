0155d9f562331ea95d15ca4c598f881f6b6e873e
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-04-16 07:54:11 +0000

    weather取得方法変更START

--- cycled_project/diary/diary_weather_report.py
+++ cycled_project/diary/diary_weather_report.py
@@ -3,9 +3,9 @@ from diary.subs.weather_report import WeatherReport
 class DiaryWeatherReport(WeatherReport):
 	def __init__(self, *args):
 		super().__init__(*args)
-		self.data["hourly"]["weather_img"]=["diary:"+name for name in self.data["hourly"]["weather_img"]]
-		self.data["daily"]["weather_img"]=["diary:"+name for name in self.data["daily"]["weather_img"]]
-		self.data["current"]["weather_img"]=["diary:"+name for name in self.data["current"]["weather_img"]]
+		# self.data["hourly"]["weather_img"]=["diary:"+name for name in self.data["hourly"]["weather_img"]]
+		# self.data["daily"]["weather_img"]=["diary:"+name for name in self.data["daily"]["weather_img"]]
+		# self.data["current"]["weather_img"]=["diary:"+name for name in self.data["current"]["weather_img"]]
 		
 if __name__=="__main__":    	
 	w=DiaryWeatherReport("関町")
