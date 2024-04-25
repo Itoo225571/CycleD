f65eceafd8a6e010c049692108dc2deaf6a9b425
UnchiFactory <153418263+UnchiFactory@users.noreply.github.com>
2024-04-25 06:51:02 +0000

    new_location更新+余計なもの削除

--- subs/test.py
+++ subs/test.py
@@ -5,6 +5,17 @@ import requests
 
 from pprint import pprint
 
+def is_empty(target_dict):
+	for value in target_dict.values():
+		if value is not None:
+			if isinstance(value,dict):
+				if not is_empty(value):
+					return False
+			else:
+				return False
+	return True
+
+
 if __name__=="__main__":    	
 	# test_list=["練馬区","東京スカイツリー","国際展示場","海城高校","幕張メッセ"]
 	# for city in test_list:
@@ -12,20 +23,35 @@ if __name__=="__main__":
 	# 	weather=WeatherReport(latitude=location.latitude,longitude=location.longitude)
 	# 	print(location)
 		# pprint(weather.today)
-	keys=["name","lat","lon","address"]
-	dict1=dict.fromkeys(["name","lat","lon","address"])
-	dict2={
-		"name":"阿佐ヶ谷",
-		"format":"json"
-	}
-	lat,lon=[43.062087 , 141.354404]
-	params={
-		"lat":lat,
-		"lon":lon,
-		"format":"geocodejson",
-		"addressdetails":1,
-	}
-	url = f"https://nominatim.openstreetmap.org/reverse"
-	res = requests.get(url=url,params=params)
+	# keys=["name","lat","lon","address"]
+	# dict1=dict.fromkeys(["name","lat","lon","address"])
+	# dict2={
+	# 	"name":"阿佐ヶ谷",
+	# 	"format":"json"
+	# }
+	# lat,lon=[43.062087 , 141.354404]
+	# params={
+	# 	"lat":lat,
+	# 	"lon":lon,
+	# 	"format":"geocodejson",
+	# 	"addressdetails":1,
+	# }
+	# url = f"https://nominatim.openstreetmap.org/reverse"
+	# res = requests.get(url=url,params=params)
 
-	pprint(res.json())
+	# pprint(res.json())
+	dict1 = {
+		"ai":None,
+		"ei":{
+			"er":None,
+			"eee":None,
+		},
+		"t":{
+			"p":None,
+			"er":{
+				"ee":None,
+				"e":None,
+			}
+		}
+	}
+	print(is_empty(dict1))
