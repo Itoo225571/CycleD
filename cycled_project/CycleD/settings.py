401e6804b2ee52107efe4c1c098a45f53d14aca3
Itoo225571 <153418263+Itoo225571@users.noreply.github.com>
2024-05-17 13:54:24 +0000

    秘密鍵まわり

--- cycled_project/CycleD/settings.py
+++ cycled_project/CycleD/settings.py
@@ -10,13 +10,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-try:
-    from .settings_secret import *
-except ImportError:
-    pass
+import environ
+import os
 
 #プロジェクトのベースフォルダを示す（今回の場合、/workspaces/MyDjango/CycleD_project）
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# .envファイルを読み込む
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
 #media用
 MEDIA_ROOT=BASE_DIR.joinpath("media")
 MEDIA_URL="/media/"
@@ -26,7 +29,8 @@ MEDIA_URL="/media/"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #Trueの時はブラウザにエラーメッセジがでる　当然本番ではFalseに
-DEBUG = True
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
