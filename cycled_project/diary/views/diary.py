34d6d91bd6bafb130a3d606f3c1c9fb5f11d76e5
admin <daigoo0601@gmail.com>
2024-08-11 21:17:49 +0900

    画像，inline変更等

--- cycled_project/diary/views/diary.py
+++ cycled_project/diary/views/diary.py
@@ -16,6 +16,29 @@ class DiaryListView(LoginRequiredMixin,generic.ListView):
     template_name = "diary/diary_list.html"
     pass
 
+# 日記作成・編集共通のクラス
+class DiaryMixin(object):
+    def form_valid(self, form, formset):
+        # formset.saveでインスタンスを取得できるように、既存データに変更が無くても更新対象となるようにする
+        for location_form in formset.forms:
+            if location_form.cleaned_data:
+                location_form.has_changed = lambda: True
+
+        # インスタンスの取得
+        diary = form.save(commit=False)
+        formset.instance = diary
+        locations = formset.save(commit=False)
+        diary.user = self.request.user
+
+        # DB更新
+        with transaction.atomic():
+            diary.save()
+            formset.instance = diary
+            formset.save()
+
+        # 処理後は詳細ページを表示
+        return redirect(diary.get_absolute_url())
+
 class DiaryNewView(LoginRequiredMixin,generic.CreateView):
     template_name = "diary/diary_new.html"
     form_class = DiaryNewForm
@@ -23,15 +46,6 @@ class DiaryNewView(LoginRequiredMixin,generic.CreateView):
     
     def form_valid(self, form):
         form.instance.user = self.request.user  # 現在のユーザーを設定
-        location_id = self.request.session.get('diary_location')
-        if location_id:
-            try:
-                location = Location.objects.get(id=location_id)
-                form.instance.location = location  # 正しいフィールド名を使用
-                print(location)
-            except Location.DoesNotExist:
-                # ログまたはエラーハンドリング
-                pass
         return super().form_valid(form)
 
 class DiaryEditView(LoginRequiredMixin,generic.UpdateView):
