a7e99e0bb28fe2c89fb8342cd43c60a7bd243711
admin <daigoo0601@gmail.com>
2024-09-09 11:33:35 +0900

    diaryphoto周り

--- cycled_project/diary/static/diary/js/append_error.js
+++ cycled_project/diary/static/diary/js/append_error.js
@@ -1,6 +1,11 @@
 function append_error(error_message) {
-    const newItem = $('<li class="list-group-item list-group-item-danger"></li>').text(error_message);
-    $('#error-list').append(newItem);
+    const existingError = $('#error-list li').filter(function() {
+        return $(this).text() === error_message;
+    });
+    if (existingError.length === 0) {
+        const newItem = $('<li class="list-group-item list-group-item-danger"></li>').text(error_message);
+        $('#error-list').append(newItem);
+    }
     $('#error-list-container').show();
 }
 function remove_error() {
