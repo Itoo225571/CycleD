e417581751937ee4026e0d167fe7a5feb751e6d4
admin <daigoo0601@gmail.com>
2024-10-04 17:26:02 +0900

    一定時間ごとにtempImage削除

--- cycled_project/diary/apps.py
+++ cycled_project/diary/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
-
+import threading
 
 class DiaryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "diary"
+
+    def ready(self):
+        from .ap_scheduler import start,delete_tempImages
+        start()
+        threading.Timer(10.0, delete_tempImages).start()  # 10秒後に実行
+        
