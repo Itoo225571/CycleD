a2775a7b3ef2bc22ba4d7a17f4180fbe971797a4
admin <daigoo0601@gmail.com>
2024-11-02 02:11:13 +0900

    アイコンサイズ変更

--- cycled_project/diary/views/weather.py
+++ cycled_project/diary/views/weather.py
@@ -12,31 +12,6 @@ from datetime import datetime
 
 class WeatherView(LoginRequiredMixin,generic.TemplateView):
     template_name="diary/weather.html"
-    def get_context_data(self, **kwargs):
-        user = self.request.user
-        context = super().get_context_data(**kwargs)
-        if user.home:
-            weather_data = self.get_weather_report(self.request)
-            context.update(weather_data)
-        return context
-    
-    # キャッシュを適用するためにdispatchにデコレーターを使用
-    @method_decorator(cache_page(60 * 15))
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-    
-    def get_weather_report(self,request):
-        lat = request.user.home.lat
-        lon = request.user.home.lon
-        img_path = static('diary_weather_report/img/')
-        weather_json = get_weather(lat,lon,dir_name = img_path,time_range=48)
-        weather = weather_json.model_dump()
-        # 位置情報を含むレスポンスを作成
-        data = {
-            'message': 'Location data received successfully.',
-            "weather_data": weather,
-        }
-        return data
 
 def ajax_location2weather(request):
     if request.method == 'POST':
