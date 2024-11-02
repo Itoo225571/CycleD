fecd42e60eacfcfb2a1dfbbb3b94ea4b988a1bfd
admin <daigoo0601@gmail.com>
2024-11-02 23:17:44 +0900

    サムネイルタッチで拡大表示

---
+++ cycled_project/diary/static/diary/js/home.js
@@ -0,0 +1,12 @@
+function zoom_image(imageUrl) {
+    const $modalImage = $('#modalImage');
+
+    $modalImage.attr('src', imageUrl);  // 画像URLを設定
+    $('#imageModalContainer').modal('show');  // Bootstrapのモーダルを表示
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    $('#imageModalContainer').on('click', function() {
+        $('#imageModalContainer').modal('hide');
+    });
+});
