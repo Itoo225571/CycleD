066d326dd4cb0c154eb4c9daa0870bc7bdaf8df3
admin <daigoo0601@gmail.com>
2024-12-31 15:17:37 +0900

    user-agentでPCとモバイルを区別して挙動を変更+キャッシュ導入

--- cycled_project/diary/views/__init__.py
+++ cycled_project/diary/views/__init__.py
@@ -1,5 +1,5 @@
 from .address import AddressUserView
-from .base import TopView,BaseView
+from .base import TopView,CacheMixin,BaseContextMixin
 from .diary import DiaryPhotoView,Photos2LocationsView,HomeView,CalendarView
 from .diary import sendDairies,delete_all_diaries,diary_edit_noPK,diary_delete_noPK,DiaryDeleteView
 from .weather import ajax_location2weather,WeatherView
--- cycled_project/diary/views/base.py
+++ cycled_project/diary/views/base.py
@@ -2,17 +2,13 @@ from typing import Any
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django_user_agents.utils import get_user_agent
 
 from ..forms import *
 
 from datetime import datetime,timedelta
-
-class BaseView(generic.TemplateView):
-    template_name="diary/base.html"
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["user"]=request.user # type: ignore
-        return context
 
 class TopView(generic.TemplateView):
     template_name="diary/top.html"
@@ -21,3 +17,16 @@ class TopView(generic.TemplateView):
         if request.user.is_authenticated:
             return redirect('diary:home')  # 'home' にリダイレクト
         return super().get(request, *args, **kwargs)
+    
+class CacheMixin:
+    @method_decorator(cache_page(60 * 15))  # 15分間キャッシュ
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+class BaseContextMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ユーザーエージェント情報をコンテキストに追加
+        context['user_agent'] = get_user_agent(self.request)
+        print(get_user_agent(self.request))
+        return context
