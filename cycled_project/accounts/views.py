fedac868b9810b6fed2c3fb8a15bf38474411275
admin <daigoo0601@gmail.com>
2025-02-12 23:19:13 +0900

    マイページ削除，設定ページ作成

--- cycled_project/accounts/views.py
+++ cycled_project/accounts/views.py
@@ -5,11 +5,13 @@ from django.shortcuts import redirect
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomLoginForm,CustomSignupForm
 from rest_framework.decorators import api_view
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from django_user_agents.utils import get_user_agent
+
+from .forms import CustomLoginForm,CustomSignupForm
+from .models import User
 from diary.models import Diary,Coin  # diaryアプリから
 
 class CustomLoginView(views.LoginView):
@@ -40,15 +42,32 @@ class CustomSignupView(views.SignupView):
         # サインアップ成功後のリダイレクト先をカスタマイズする場合はここで設定
         return super().get_success_url()
     
-class UserProfileView(LoginRequiredMixin,generic.TemplateView):
-    template_name="accounts/mypage.html"
+# class UserProfileView(LoginRequiredMixin,generic.TemplateView):
+#     template_name="accounts/mypage.html"
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         user = self.request.user  # 現在ログインしているユーザーを取得
+#         context['user'] = user  # ユーザー情報をテンプレートに渡す
+#         diary_all = Diary.objects.filter(user=user)
+#         context['diary_count'] = diary_all.count()  # ユーザーの持っている日記の数をカウント
+#         context['diary_count_ontheday'] = diary_all.filter(rank=0).count()
+#         return context
+
+class UserSettingView(LoginRequiredMixin,generic.UpdateView):
+    model = User
+    template_name="accounts/setting.html"
+    fields = ['username', 'email', 'icon']
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user  # 現在ログインしているユーザーを取得
         context['user'] = user  # ユーザー情報をテンプレートに渡す
-        context['diary_count'] = Diary.objects.filter(user=user).count()  # ユーザーの持っている日記の数をカウント
-        context['coin'] = Coin.objects.filter(user=user).first()  # ユーザーに関連するCoinを取得（1つだけ）
+        diary_all = Diary.objects.filter(user=user)
+        context['diary_count'] = diary_all.count()  # ユーザーの持っている日記の数をカウント
+        context['diary_count_ontheday'] = diary_all.filter(rank=0).count()
         return context
+    def get_object(self, queryset=None):
+        # 常に現在ログイン中のユーザーを返す
+        return self.request.user
 
 class UserEditView(LoginRequiredMixin,generic.UpdateView):
     pass
