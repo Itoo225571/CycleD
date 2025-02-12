b1fb973802d8f622c21d6ba180fed5bc6a88dd32
admin <daigoo0601@gmail.com>
2025-02-12 23:19:13 +0900

    マイページ削除，設定ページ作成

--- cycled_project/accounts/urls.py
+++ cycled_project/accounts/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
 
-    path("user/",views.UserProfileView.as_view(),name="mypage"),
+    # path("user/",views.UserProfileView.as_view(),name="mypage"),
+    path("setting/",views.UserSettingView.as_view(),name="setting"),
     path("user/edit/",views.UserEditView.as_view(),name="user_edit"),
     path("user/delete/",views.user_delete,name="user_delete"),
 ]
