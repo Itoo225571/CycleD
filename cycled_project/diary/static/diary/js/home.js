46d1fb5a4c27a7b6c416b577e8b85091faac14aa
admin <daigoo0601@gmail.com>
2025-02-22 21:51:22 +0900

    homeのthumbnail調整&goodの外観追加

--- cycled_project/diary/static/diary/js/home.js
+++ cycled_project/diary/static/diary/js/home.js
@@ -7,10 +7,17 @@ function zoom_image(imageUrl,angle) {
 }
 
 $(document).ready(function() {
-    $('.card').each(function(index, card) {
-        adjust_imgs($(card),'max');
+    $('.card-thumbnail').each(function(index, thumbnail) {
+        adjust_imgs($(thumbnail),'max');
     });
     $('#imageModalContainer .modal-content').on('click', function() {
         $('#imageModalContainer').modal('hide');
+    });
+
+    // Bootstrapのタブが切り替わる際に発火するイベント
+    $('.diary-list-field .nav-item').on('click', function() {
+        $('.card-thumbnail').each(function(index, thumbnail) {
+            adjust_imgs($(thumbnail),'max');
+        });
     });
 });
