62ea94be69dfc9301a6a65dd02a5ddb580494666
admin <daigoo0601@gmail.com>
2025-03-19 13:06:45 +0900

    weather report変更

--- cycled_project/diary/views/__init__.py
+++ cycled_project/diary/views/__init__.py
@@ -3,4 +3,4 @@ from .address import address_search,get_current_address
 from .base import TopView,CacheMixin
 from .diary import DiaryPhotoView,Photos2LocationsView,HomeView,CalendarView,DiaryDeleteView,MapView
 from .diary import sendDairies,delete_all_diaries,diary_edit_noPK,diary_delete_noPK,good_for_diary
-from .weather import ajax_location2weather,WeatherView
+from .weather import WeatherView
