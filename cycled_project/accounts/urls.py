875040dca975088618d3ca2c73fee19a646da4bc
admin <daigoo0601@gmail.com>
2025-03-21 01:12:02 +0900

    email画面変更途中

--- cycled_project/accounts/urls.py
+++ cycled_project/accounts/urls.py
@@ -10,12 +10,15 @@ urlpatterns = [
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
     path("leave/",views.UserLeaveView.as_view(),name="leave"),
 
+    path("",views.UserSettingView.as_view(),name="setting"),
+
     # path("user/",views.UserProfileView.as_view(),name="mypage"),
-    path("setting/",views.UserSettingView.as_view(),name="setting"),
-    path("user/edit/",views.UserEditView.as_view(),name="user_edit"),
+    # path("user/edit/",views.UserEditView.as_view(),name="user_edit"),
+
+    path("email/",views.CustomEmailView.as_view(),name="email"),
 
     path("password/change/",views.CustomPasswordChangeView.as_view(),name="password_change"),
     path("password/set/",views.CustomPasswordSetView.as_view(),name="password_set"),
 
-    path("3rdparty",views.CustomConnectionsView.as_view(),name="connections"),
+    path("3rdparty/",views.CustomConnectionsView.as_view(),name="connections"),
 ]
