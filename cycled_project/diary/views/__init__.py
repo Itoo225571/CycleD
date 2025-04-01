5a22830d02ba7cf58abe41b19c884df2f8d568cb
admin <daigoo0601@gmail.com>
2025-04-01 21:07:21 +0900

    diaryをキャッシュ

--- cycled_project/diary/views/__init__.py
+++ cycled_project/diary/views/__init__.py
@@ -1,6 +1,8 @@
 from .address import AddressUserView
 from .address import address_search,get_current_address
 from .base import TopView,CacheMixin
-from .diary import DiaryPhotoView,Photos2LocationsView,HomeView,CalendarView,DiaryDeleteView,MapView
-from .diary import sendDairies,delete_all_diaries,diary_edit_noPK,diary_delete_noPK,good_for_diary
+from .diary import DiaryPhotoView,Photos2LocationsView,CalendarView,DiaryDeleteView,MapView
+from .diary import sendDairies,delete_all_diaries,diary_edit_noPK,diary_delete_noPK
 from .weather import WeatherView
+
+from .home import HomeView,good_for_diary
---
+++ cycled_project/diary/views/cache_and_session.py
@@ -0,0 +1,50 @@
+from django.db.models import Prefetch
+from django.core.cache import cache
+
+from ..models import Diary,Location,TempImage,Good
+
+def update_diaries(request,whose=['mine','public']):
+    key = 'diaries_for_display'
+    diaries = {}
+
+    # 自分の情報はsessionに保存
+    if 'mine' in whose:
+        diaries_mine = Diary.objects.filter(user=request.user)
+        diary_count = diaries_mine.count()
+        diary_count_ontheday = diaries_mine.filter(rank=0).count()
+        data = {
+            'diaries_mine': diaries_mine,
+            'count': diary_count,
+            'count_ontheday': diary_count_ontheday
+        }
+        request.session[key] = data
+        diaries.update(data)
+
+    # 全体の情報はcacheに保存
+    if 'public' in whose:
+        thumbnail_location = Prefetch(
+            'locations',
+            queryset=Location.objects.filter(is_thumbnail=True),
+            to_attr='thumbnail_locations'
+        )
+        diaries_public = (
+            Diary.objects.filter(is_public=True)
+            # .exclude(user=request.user)   # 除去しない
+            .order_by('-date_last_updated', '-date')
+            .prefetch_related(thumbnail_location, 'user')
+        )
+        data = {
+            'diaries_public': diaries_public,
+        }
+        cache.set(key, data, timeout=3600)  # 1時間のキャッシュ
+        diaries.update(data)
+    return diaries
+
+def get_diaries(request,whose=['mine','public']):
+    key = 'diaries_for_display'
+    request.session.get(key,None) or print('ない')
+    cache.get(key, None) or print('NO')
+    # whoseの中に対象の名前があったらそれをセッションもしくはキャッシュにより取得(失敗したらupdateを使う)
+    session_data = (request.session.get(key,None) or update_diaries(request, ['mine'])) if 'mine' in whose else {}
+    cached_data = (cache.get(key, None) or update_diaries(request, ['public'])) if 'public' in whose else {}
+    return {**session_data, **cached_data}
