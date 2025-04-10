e37a72467c9fa89d9a442ae892a15f92addb0c21
admin <daigoo0601@gmail.com>
2025-04-10 18:17:04 +0900

    Coin周り改修

--- cycled_project/diary/views/diary_photo.py
+++ cycled_project/diary/views/diary_photo.py
@@ -115,7 +115,7 @@ class DiaryPhotoView(LoginRequiredMixin, generic.FormView):
                     if id:
                         if datetime.now().date() == temp_image.date_photographed.date() == temp_image.date_lastModified.date():
                             location.diary.rank = 0
-                            self.request.user.coin.add()
+                            # self.request.user.coin.add()
                             location.diary.save()
                     location.full_clean()  # バリデーションを実行
                     location.save()
